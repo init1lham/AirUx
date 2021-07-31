18f1bb0b5fb88a29b221d149bb167c319bd49286
Ilham Isayev <88136627+init1lham@users.noreply.github.com>
2021-07-31 17:46:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+sudo apt-get install cmake
+sudo apt-get install libssl-dev libavahi-compat-libdnssd-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-libav
+sudo apt-get install gstreamer1.0-vaapi
 mkdir build
 cd build
 cmake ..
