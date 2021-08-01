f76ab4bae038ad6949f33aa6a9bbaa7595dade2a
Ilham Isayev <88136627+init1lham@users.noreply.github.com>
2021-08-01 07:22:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-sudo apt-get install cmake -y
-sudo apt-get install libssl-dev libavahi-compat-libdnssd-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-libav -y
-sudo apt-get install gstreamer1.0-vaapi -y
-mkdir build
-cd build
+tput setaf 0;
+sudo apt-get install cmake gstreamer1.0-vaapi libssl-dev libavahi-compat-libdnssd-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-libav -y 
+mkdir build 
+cd build 
 cmake ..
-make
-sudo cp AirUx /usr/local/bin
-ehco "AirUx added to path. You can run sudo AirUx to access AirUx from everywhere."
-sudo avahi-daemon
-./AirUx
-
+make 
+sudo cp AirUx /usr/local/bin                     
+sudo avahi-daemon 
+tput setaf 1;
+figlet "AirUx is added to path. You can access AirUx from anywhere."
+tput setaf 2;
+figlet "AirUx is Running"
+tput setaf 3;
+./AirUx 
